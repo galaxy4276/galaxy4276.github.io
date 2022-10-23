e453973eb74cdb910c5f50b5e5164f7983269352
최은기 <galaxyhi4276@gmail.com>
2022-10-24 06:30:42 +0900

    feat: 웹 폰트 ssr 적용

---
+++ src/gatsby-ssr.js
@@ -0,0 +1,20 @@
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <link
+      rel="preload"
+      href="./fonts/JetBrainsMono-Medium.woff2"
+      as="font"
+      type="font/woff2"
+      crossOrigin="anonymous"
+      key="interFont"
+    />,
+    <link
+      rel="preload"
+      href="./fonts/Pretendard-Medium.woff2"
+      as="font"
+      type="font/woff2"
+      crossOrigin="anonymous"
+      key="interFont"
+    />
+  ]);
+};
