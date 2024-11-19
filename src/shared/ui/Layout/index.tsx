2f76c6d144fbb73c9040df679aaa48f911488a30
galaxy4276 <galaxy4276@users.noreply.github.com>
2024-11-19 01:11:42 +0000

    deploy: 09ab2d85db3cdf62145c92b05e28467b769e6f06

--- src/shared/ui/Layout/index.tsx
+++
@@ -1,9 +0,0 @@
-import { FC, PropsWithChildren } from "react";
-
-export const Layout: FC<PropsWithChildren> = ({ children }) => (
-  <div className="w-[100dvw] h-[100dvh] overflow-y-scroll overflow-x-hidden app-background flex justify-center">
-    <main className="px-[16px] lg:px-0 w-full max-w-screen-sm md:max-w-screen-md lg:max-w-[900px] h-[100dvh] resize-anim">
-      {children}
-    </main>
-  </div>
-);
